3fe3b8a34b189d12dafe2005d8e5d17ed13a2f66
Felix Leipold <felix.leipold@gmail.com>
2012-06-14 10:18:12 +0200

    SSH-OPTS for smoketests

--- smoke-tests.sh
+++ smoke-tests.sh
@@ -4,4 +4,4 @@ set -e
 key=$1
 slice=$2
 
-ssh -i "$key" ubuntu@$slice './one-click-cd-demo/server-scripts/smoke-tests-local.sh'
+ssh $SSH_OPTS -i "$key" ubuntu@$slice './one-click-cd-demo/server-scripts/smoke-tests-local.sh'
