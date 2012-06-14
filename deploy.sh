1c03eb85021260c252c6e246824ad9f243b91716
Felix Leipold <felix.leipold@gmail.com>
2012-06-14 10:15:47 +0200

    Introducing SSH_OPTS

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ slice=$2
 
 tarfile="one-click-cd-demo.tgz"
 
-ssh -i "$key" ubuntu@$slice 'rm -rf ~/one-click-cd-demo'
-ssh -i "$key" ubuntu@$slice 'mkdir ~/one-click-cd-demo'
-scp -i "$key" dist/$tarfile ubuntu@$slice:~/one-click-cd-demo
-ssh -i "$key" ubuntu@$slice "cd ~/one-click-cd-demo && tar xfz $tarfile"
+ssh $SSH_OPTS -i "$key" ubuntu@$slice 'rm -rf ~/one-click-cd-demo'
+ssh $SSH_OPTS -i "$key" ubuntu@$slice 'mkdir ~/one-click-cd-demo'
+scp $SSH_OPTS -i "$key" dist/$tarfile ubuntu@$slice:~/one-click-cd-demo
+ssh $SSH_OPTS -i "$key" ubuntu@$slice "cd ~/one-click-cd-demo && tar xfz $tarfile"
