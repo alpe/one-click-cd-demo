adf364aea26533bab7d4f7917122630f02ac29dc
Matt <matt@matthias.(none)>
2012-06-14 13:02:03 +0200

    added the build number to the html -- fixed the test

--- build.sh
+++ build.sh
@@ -9,7 +9,18 @@ node_modules/jasmine-node/bin/jasmine-node spec
 
 echo "BUILD: packaging"
 mkdir dist
-tar cfz dist/one-click-cd-demo.tgz server-scripts static
+cp -r server-scripts static dist/
+cd dist
+
+build_number="host:`hostname`"
+[ -z $GO_PIPELINE_COUNTER ] || build_number="go:$GO_PIPELINE_COUNTER"
+git_version=`git log | head -1 | awk '{print $2}'`
+build_number="$build_number - git:$git_version - `date +"%m-%d-%Y @ %T"`"
+echo build number = $build_number
+
+sed -i -e "s/\${BUILD_NUMBER}/$build_number/g" static/*html
+
+tar cfz one-click-cd-demo.tgz *
 
 echo "BUILD: done"
 exit 0
