3a9b746bc66d0ecc9e3488ec96c016c3f72f3e03
Felix Leipold <felix.leipold@gmail.com>
2012-06-14 11:56:05 +0200

    Make deploy script stop and start server

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,11 @@ slice=$2
 
 tarfile="one-click-cd-demo.tgz"
 
+(./stop.sh $key $slice)
+
 ssh $SSH_OPTS -i "$key" ubuntu@$slice 'rm -rf ~/one-click-cd-demo'
 ssh $SSH_OPTS -i "$key" ubuntu@$slice 'mkdir ~/one-click-cd-demo'
 scp $SSH_OPTS -i "$key" dist/$tarfile ubuntu@$slice:~/one-click-cd-demo
 ssh $SSH_OPTS -i "$key" ubuntu@$slice "cd ~/one-click-cd-demo && tar xfz $tarfile"
+
+./start.sh $key $slice
