0c2e22cba25db884ab4926b4b3a1b5e46079aa1e
Matt <matt@matthias.(none)>
2012-06-26 14:06:26 +0200

    fixing it

--- spec/application.spec.js
+++ spec/application.spec.js
@@ -4,7 +4,7 @@ requirejs(['static/application.js'],
 		function (app) {
 			describe("one-click CD demo", function() {
 				it("says hello", function() {
-					expect(app.hello_world()).toEqual("Hello world?");
+					expect(app.hello_world()).toEqual("Hello world!");
 				});
 			});
 		});
